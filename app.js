56ebfda67ee9e3b764f95b25b8043eab2986cde8
ran <ranchen27056@gmail.com>
2021-04-29 14:56:45 +0800

    add sessions and coockies

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-
-mongoose.connect("mongodb+srv://ran:050627@ran.agxxa.mongodb.net/blogDB?retryWrites=true&w=majority", {useNewUrlParser:true});
+const session = require("express-session");
+const passport = require("passport");
+//passportlocalMongoose require passport-local dependency while don't have to be explictly require here
+const passportLocalMongoose=require("passport-local-mongoose");
 const app = express();
+// const md5 = require("md5");
+mongoose.connect("mongodb+srv://ran:050627@ran.agxxa.mongodb.net/blogDB?retryWrites=true&w=majority", {useNewUrlParser:true});
+mongoose.set("useCreateIndex",true);
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+  }));
+app.use(passport.initialize());
+app.use(passport.session());
+const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 var _ = require('lodash');
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -18,6 +35,26 @@ const postSchema = {
     content: String
 };
 const Post = mongoose.model("Post",postSchema);
+const userSchema  =new mongoose.Schema ( {
+    email: String,
+    password: String
+});
+userSchema.plugin(passportLocalMongoose);
+
+//only password feild , mongoose will encrypt when you save, decrypt when you find
+//userSchema.plugin(encrypt, { secret: process.env.secret, encryptedFields:['password'] });
+//remove this plugin to try md5
+const User = mongoose.model("User", userSchema);
+passport.use(User.createStrategy());
+//serialize create cookie,
+passport.serializeUser(User.serializeUser());
+//use pas
+passport.deserializeUser(User.deserializeUser());
+// const admin = new User({
+//     email:"384961093@qq.com",
+//     password: "1234"
+// });
+// admin.save();
 
 app.get("/",function(req,res) {
     Post.find({},function(err,posts) {
@@ -68,6 +105,101 @@ app.get("/login",(req,res) =>{
 app.get("/register",(req,res) =>{
     res.render("register");
 
+});
+//register user
+// app.post("/register", (req,res)=>{
+//     bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+//         // Store hash in your password DB.
+//         const newUser  = new User({
+//             email : req.body.username,
+//             password : hash
+//         });
+//         newUser.save(function(err){
+//             if (err) {
+//                 console.log(err);
+//             }
+//             else {
+//                 res.render("secrets");
+//             }
+//         });
+//     });
+//     // const newUser  = new User({
+//     //     email : req.body.email,
+//     //     password : md5(req.body.password)
+//     // });
+    
+// });
+//login route check valid user
+// app.post("/login",(req,res) =>{
+//     const username = req.body.username;
+//     const password = req.body.password;
+    
+//     User.findOne({email:username},function(err,founduser) {
+//         if (err) {
+//             console.log(err);
+//         }
+//         else {
+//             if (founduser) {
+//                 // if (founduser.password === password) {
+//                 //     res.render("secrets");
+//                 // }
+//                 // the first parameter is the value passed in, the second parameter is the value stored in the database
+//                 bcrypt.compare(password, founduser.password, function(err, result) {
+//                     // result == true
+//                     if (result === true) {
+//                         res.render("secrets");
+//                     }
+//                     else {
+//                         console.log("wrong password");
+//                     }
+
+//                 });
+//             }
+//         }
+//     });
+// });
+//check session if user is log in
+app.get("/secrets",(req,res)=> {
+    if (req.isAuthenticated() ){
+        res.render("secrets");
+    }
+    else {
+        res.redirect("/login");
+    }
+});
+app.post("/register",function(req,res) {
+    User.register({username:req.body.username}, req.body.password,function(err,user) {
+        if(err) {
+            console.log(err);
+            res.redirect("/register");
+        }
+        else{
+            //authenticate this session, save your session id, coockie saved,here session is set to end when you close your browser
+            passport.authenticate("local")(req,res,function()  {
+                res.redirect("/secrets");
+            });
+        }
+    });
+});
+app.post("/login",(req,res) => {
+    const user = new User ({
+        email: req.body.username,
+        password: req.body.password
+    });
+    req.login(user,function(err) {
+        if(err) {
+            console.log(err);
+        }else {
+            passport.authenticate("local") (req,res,function() {
+                res.redirect("/secrets");
+            });
+        }
+    });
+});
+app.get("/logout", (req,res) => {
+    //deauthenticate user and log out session
+    req.logout();
+    res.redirect("/");
 });
 app.listen(3001,()=>{
     console.log("Example app listening at 3001");
